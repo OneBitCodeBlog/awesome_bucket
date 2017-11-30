6b3d64115d3eff12caef825ffe1b54df8e0f98b7
Daniel Moreto <dfmoreto@gmail.com>
2017-11-30 18:07:57 -0200

    Configure :create action and list files of Bucket on :show view

--- app/controllers/file_uploads_controller.rb
+++ app/controllers/file_uploads_controller.rb
@@ -2,4 +2,10 @@ class FileUploadsController < ApplicationController
   def new
     @bucket = Bucket.find(params[:bucket_id])
   end
+
+  def create
+    @bucket = Bucket.find(params[:bucket_id])
+    @bucket.files.attach(params[:bucket][:files])
+    redirect_to bucket_path(@bucket)
+  end
 end
